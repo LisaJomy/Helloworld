04bec96d9384e55c9de2b2efd4135e2ba944dd28
Lisa Jomy <lisa.selenium@gmail.com>
2020-08-25 05:39:39 -0400

    this is new comments

--- src/Hello.java
+++ src/Hello.java
@@ -5,9 +5,11 @@ public class Hello {
 public static void main(String [] args){
 
     System.out.println(" hello World");
-
+// please add more code here
        Product product = new Product( "Keyboard");
        System.out.println(product.getName());
+
+ // this is the final commit
     }
 
 }
