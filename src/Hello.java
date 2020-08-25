2a00e515ca420801176efa81d2dd64b34990171d
Lisa Jomy <lisa.selenium@gmail.com>
2020-08-25 05:24:48 -0400

    This is helloworld commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+import com.amigoscode.modal.Product;
+
+public class Hello {
+
+public static void main(String [] args){
+
+    System.out.println(" hello World");
+
+       Product product = new Product( "Keyboard");
+       System.out.println(product.getName());
+    }
+
+}
